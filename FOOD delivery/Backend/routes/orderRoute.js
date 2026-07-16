8e53728388cc229e37346f6a92027f0f8255aa25
Nitin Ghildiyal <ghildiyalnitin067@gmail.com>
2026-07-16 20:24:39 +0530

    feat(backend): implement order CRUD API endpoints

--- FOOD delivery/Backend/routes/orderRoute.js
+++ FOOD delivery/Backend/routes/orderRoute.js
@@ -1,13 +1,14 @@
-import { placeOrder , verifyOrder ,userOrders} from "../controllers/orderController.js";
+import { placeOrder, verifyOrder, userOrders, listOrders, updateStatus, removeOrder } from "../controllers/orderController.js";
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
 
-
-
 const orderRouter = express.Router();
 
-orderRouter.post("/place",authMiddleware,placeOrder);
-orderRouter.post("/verify",authMiddleware,verifyOrder);
-orderRouter.post("/userorders",authMiddleware,userOrders);
+orderRouter.post("/place", authMiddleware, placeOrder);
+orderRouter.post("/verify", authMiddleware, verifyOrder);
+orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updateStatus);
+orderRouter.post("/remove", removeOrder);
 
 export default orderRouter;
