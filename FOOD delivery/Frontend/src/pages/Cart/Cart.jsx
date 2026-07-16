d9726b75f9159ce4994ee20f14e15408e8fbe715
Nitin Ghildiyal <ghildiyalnitin067@gmail.com>
2026-07-16 21:03:32 +0530

    fix(frontend): handle Cloudinary URLs in cart image rendering

--- FOOD delivery/Frontend/src/pages/Cart/Cart.jsx
+++ FOOD delivery/Frontend/src/pages/Cart/Cart.jsx
@@ -38,7 +38,7 @@ const Cart = () => {
                 return (
                   <div key={item._id}>
                     <div className="cart-items-title cart-items-item">
-                      <img src={url+"/images/"+item.image} alt={item.name} />
+                      <img src={item.image.startsWith("http") ? item.image : url+"/images/"+item.image} alt={item.name} />
                       <p>{item.name}</p>
                       <p>${item.price}</p>
                       <div className="cart-quantity-controls">
